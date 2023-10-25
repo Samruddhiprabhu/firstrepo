d1ea50149b6fc4c7b6b043a382044364e2aee1ac
Samruddhiprabhu <samrudhiprabhusmg@gmail.com>
2023-10-25 12:02:38 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,1 +1,14 @@
-simple calculator
+#simple calculator
+a=int(input('Enter the first number:'))
+b=int(input("Enter the second number:"))
+c=input("Enter the operator:")
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=="/":
+    print(a/b)
+else:
+    print('Enter the correct operator!!!')
