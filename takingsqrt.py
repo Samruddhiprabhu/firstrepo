0194ec1e4a2a50adf7fbeaebc31399d085459f56
Samruddhiprabhu <samrudhiprabhusmg@gmail.com>
2023-10-25 13:05:06 +0530

    Create takingsqrt.py

---
+++ takingsqrt.py
@@ -0,0 +1,11 @@
+'''#by exponentiation
+num=int(input("Enter the value to get its squareroot:"))
+sqrt=num**0.5
+print('squareroot of the num:',sqrt)'''
+
+
+#by importing module
+from math import sqrt
+num=int(input("Enter the value to get its squareroot:"))
+sr=sqrt(num)
+print('square root of the num:',sr)
